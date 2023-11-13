04032abe64505949cd4d710e5825fcfdcd318966
Endre Svensen <mail@endresvensen.no>
2023-11-13 16:57:44 +0100

    Added categories sidebar

---
+++ script.js
@@ -0,0 +1,21 @@
+fetch('pagelist.json')
+    .then(response => response.json())
+    .then(data => {
+        const overview = document.createElement('div');
+        overview.id = 'overview';
+        document.body.prepend(overview);
+        const currentPage = window.location.pathname.split('/').pop();
+        data.files.forEach(file => {
+            const fileName = file.replace('.html', '').replace('_', ' ');
+            const listItem = document.createElement('li');
+            const link = document.createElement('a');
+            link.href = `${file}`;
+            link.textContent = fileName;
+            listItem.appendChild(link);
+            if (file === currentPage) {
+                listItem.classList.add('current');
+            }
+            overview.appendChild(listItem);
+        });
+    })
+    .catch(error => console.error('Error:', error));
