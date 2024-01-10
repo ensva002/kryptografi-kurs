b6e4df9b55a51a3f5a4e0a1d9dbfb8712d8cee7d
ensva002 <144826940+ensva002@users.noreply.github.com>
2024-01-10 13:28:28 +0100

    ?

--- toolmac.sh
+++ toolmac.sh
@@ -3,6 +3,9 @@
 dir="markdown" 
 siste_endring=""
 
+echo "Kjører i $dir, åpne din favoritt markdown editor å begynn å skriv :)"
+macdown
+
 while true; do
     filepath=$(fswatch -1 $dir)  
     echo "Det har skjedd endringer i filene: $filepath"
