f77157a3331ed3939820d025110930d47a988179
ensva002 <144826940+ensva002@users.noreply.github.com>
2024-02-24 18:36:23 +0100

    Update BSDrunBeforeCommit.sh

--- BSDrunBeforeCommit.sh
+++ BSDrunBeforeCommit.sh
@@ -13,7 +13,7 @@ do
     # Extract the filename from the filepath
     filename=$(basename "$filepath" .md)
 
-    pandoc -f markdown $filepath > $dir/$filename.html
+    pandoc -f markdown+emoji $filepath > $dir/$filename.html
     gsed -E -i 's/<pre\sclass="([^"]*)">/<pre class="language-\1">/g' $dir/$filename.html
     
     # Replace underscores with spaces
